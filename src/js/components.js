176518a32c4c95f0661d16bc3d7a0946cfecbe5f
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-28 15:45:08 +0200

    Added svg components

--- src/js/components.js
+++ src/js/components.js
@@ -1,10 +1,22 @@
+import linux from '../svg/linux.svg';
+import mobile from '../svg/mobile.svg';
+import ps4 from '../svg/ps4.svg';
 import search from '../svg/search.svg';
+import nintendo from '../svg/switch.svg';
+import windows from '../svg/windows.svg';
+import xbox from '../svg/xbox.svg';
 
 class Components {
 
     constructor() {
         this.fileNames = {
+            'linux': linux,
+            'mobile': mobile,
+            'ps4': ps4,
             'search': search,
+            'switch': nintendo,
+            'windows': windows,
+            'xbox': xbox,
         }
     }
 
