f62412eb3391ce7eb37f4c773d8d951fa14a859c
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-29 13:36:10 +0200

    Modified PageDetail

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -12,14 +12,61 @@ const pagedetail = (argument = "") => {
             fetch(`${finalURL}`)
                 .then((response) => response.json())
                 .then((response) => {
-                    let { name, released, description } = response;
 
-                    let articleDOM = document.querySelector(".page-detail .article");
+                    let articleDOM = document.querySelector(".page-detail");
 
-                    articleDOM.querySelector("h1.title").innerHTML = name;
-                    articleDOM.querySelector("p.release-date span").innerHTML = released;
-                    articleDOM.querySelector("p.description").innerHTML = description;
-                });
+                    articleDOM.innerHTML = `
+                        <div id="bigImg" class="img-lg d-flex justify-content-between align-items-center" style="background: transparent url('${response.background_image}') center center no-repeat; background-size: cover;">
+                            <div class="img-lg-btn-group">
+                                <a href="${response.website}" class="img-lg-btn">Check website</a>
+                            </div>
+                        </div>
+                        <div class="article">
+                            <div class="title-group d-flex justify-content-between">
+                                <h1 class="h1 title">${response.name}</h1>
+                            <div class="title-group-votes">${response.rating}/${response.rating_top} -  ${response.ratings_count} votes</div>
+                        </div>
+
+                        <p class="description">${response.description}</p>
+
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-3 release-date">${response.released}</div>
+                        </div>
+
+                        <h2 class="h2">BUY</h2>
+                        <div id="stores" class="mb-5">
+                        </div>
+                        <h2 class="h2">Trailer</h2>
+                        <video id="trailer" class="mb-5"></div>
+
+                        <h2 class="h2">Screenshots</h2>
+                        <div class="row mb-5">
+                            <div class="col-12 col-sm-6">
+                                <a href="" class=""></a>
+                            </div>
+                        </div>
+
+                        <h2 class="h2">Youtube</h2>
+                        <div class="row">
+                            <div class="col-12 col-sm-6"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4"></div>
+                        </div>
+
+                        <h2 class="h2">Similar games</h2>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <a href="" class=""></a>
+                                <h3 class="h3"></h3>
+                                <ul class="platform-list">
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`;
+            });
+                
         };
 
         fetchGame("https://api.rawg.io/api/games/", cleanedArgument);
@@ -28,67 +75,7 @@ const pagedetail = (argument = "") => {
     const render = () => {
         let pageContent = document.getElementById("pageContent");
         pageContent.innerHTML = `
-            <section class="page-detail">
-                <div class="img-lg d-flex justify-content-between align-items-center">
-                    <div class="img-lg-btn-group">
-                        <a href="" class="mg-lg-btn"></a>
-                    </div>
-                </div>
-                <div class="article">
-                    <div class="title-group d-flex justify-content-between">
-                        <h1 class="h1 title"></h1>
-                        <div class="title-group-votes">/ -  votes</div>
-                    </div>
-                    <p class="welcome-content">textes ici</p>
-
-                    <p class="welcome-content"><strong></strong></p>
-                    <p class="welcome-content">textes ici</p>
-
-                    <p class="welcome-content"><strong></strong></p>
-                    <p class="welcome-content">textes ici</p>
-
-                    <div class="row">
-                        <div class="col-12 col-sm-6 col-md-3">Release...</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 col-sm-6">Release...</div>
-                    </div>
-
-                    <h2 class="h2">BUY</h2>
-                    <div class="mb-5">
-                        <a href="" class=""></a>
-                        <a href="" class=""></a>
-                    </div>
-                    <h2 class="h2">Trailer</h2>
-                    <video class="mb-5"></div>
-
-                    <h2 class="h2">Screenshots</h2>
-                    <div class="row mb-5">
-                        <div class="col-12 col-sm-6">
-                            <a href="" class=""></a>
-                        </div>
-                    </div>
-
-                    <h2 class="h2">Youtube</h2>
-                    <div class="row">
-                        <div class="col-12 col-sm-6"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 col-sm-6 col-md-4"></div>
-                    </div>
-
-                    <h2 class="h2">Simular games</h2>
-                    <div class="row">
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <a href="" class=""></a>
-                            <h3 class="h3"></h3>
-                            <ul class="platform-list">
-                                <li></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>`;
+            <section class="page-detail"></section>`;
 
         preparePage();
     };
